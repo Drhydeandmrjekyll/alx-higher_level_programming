3eac0c030e0407ff2f4f0eaa69aa08a9ffbe1508
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-15 15:57:32 -0700

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- This script creates the 'second_table' if it doesn't exist and inserts records.
+-- The script is designed to create a maximum of 4 records.
+
+-- Create table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- Insert records into the 'second_table'
+-- Script should create maximum of 4 records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+
