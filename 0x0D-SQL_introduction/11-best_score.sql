929c2edffff562f69cbcd755915bdb48b5ea3ac3
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-15 16:09:00 -0700

    11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- This query retrieves scores and names from 'second_table' where score's greater than or equal to 10,
+-- and orders the results in descending score
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
