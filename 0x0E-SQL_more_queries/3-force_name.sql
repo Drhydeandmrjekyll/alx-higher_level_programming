3d6da91d756eb1e10a2c53371bc1cada11296210
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-16 14:53:23 -0700

    always_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Creates or checks existence of a table 'force_name' with columns 'id' (INT) and 'name' (VARCHAR(256), NOT NULL).
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+
