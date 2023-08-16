030575958b7400c73cffe3a930a79f10901e3568
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-15 17:32:50 -0700

    #0_temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
 -- Query to display average temperature by city ordered by temperature (descending)
 SELECT city, AVG(temperature) AS average_temperature
-FROM your_table_name -- Replace with the actual table name
+-- Replace with the actual table name
+FROM your_table_name
 GROUP BY city
 ORDER BY average_temperature DESC;
