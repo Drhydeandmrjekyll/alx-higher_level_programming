dc20ab394c9c260e1b67e38c09b863e94e2bf6ed
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-16 16:15:59 -0700

    Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Retrieves genres of the TV show 'Dexter' by joining tables and filtering results, sorted by genre name.
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
+
