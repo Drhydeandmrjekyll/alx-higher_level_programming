8c0e86d18231eafb2f25500a3a744d7a3f4f9c96
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-16 12:33:26 -0700

    reduced_restrictions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- Create a new user 'user_0d_1' if it doesn't already exist, with a password 'user_0d_1_pwd',
 -- allowing them to connect from 'localhost
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to the user 'user_0d_1' when accessing any database and table
 -- using the 'user_0d_1'@'localhost' login
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 -- Update the privilege table to reflect the changes made
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
