98197f964ffeb013b946ffe783bf82630dc0bd90
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-16 09:32:11 -0700

    my_privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Retrieve and display grants for 'user_0d_1' when accessing from 'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Retrieve and display grants for 'user_0d_2' when accessing from 'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
