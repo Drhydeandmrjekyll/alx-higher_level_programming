14632dabdf5e9ced3c9e44fcccee8ff1aa86c1e6
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-15 17:16:18 -0700

    Go-UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Switch to 'hbtn_0c_0' database
+USE `hbtn_0c_0`
+-- Convert character set and collation for the 'first_table' table
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
