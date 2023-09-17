ea49eebd86ce6016532cbd47b0e9c735e8f67dc2
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-09-17 14:39:20 -0700

    city-relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Module that adds a city and its associated state\
-        to a MySQL database using SQLAlchemy."""
+"""Module which adds city and its associated state\
+        to MySQL database using SQLAlchemy."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,27 +8,27 @@ from relationship_state import State
 from relationship_city import Base, City
 
 if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
+    # Create SQLAlchemy engine using provided MySQL credentials
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    # Create the database tables based on the defined models
+    # Create database tables based on defined models
     Base.metadata.create_all(engine)
 
-    # Create a session factory
+    # Create session factory
     Session = sessionmaker(bind=engine)
 
-    # Create a session object
+    # Create session object
     session = Session()
 
-    # Create a new city and its associated state
+    # Create new city and its associated state
     state = State(name="California")
     city = City(name="San Francisco", state=state)
 
-    # Add the new city and associated state to the session
+    # Add new city and associated state to session
     session.add(state)
     session.add(city)
 
-    # Commit the session to persist the changes in the database
+    # Commit session to persist changes in database
     session.commit()
