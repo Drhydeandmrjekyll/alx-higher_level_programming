bd2b648cebe6d8ecf542e608693a52b9f1e2c245
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-09-28 14:52:37 -0700

    only_method

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #script to check response in bytes
-curl -s "$1"|wc -c
+curl -s "$1" | wc -c
 
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script sends DELETE request to URL provided as first argument using curl,
+curl -sX DELETE "$1"
+
