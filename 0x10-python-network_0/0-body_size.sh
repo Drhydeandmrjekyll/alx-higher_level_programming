271706e62a668b28dd308be24dce7fa8e067a2d1
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-09-28 13:43:30 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,19 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+#script to check response in bytes
 
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Get URL from command line argument
-URL="$1"
-
-# Send GET request to URL and store response in temporary file
-RESPONSE=$(curl -s "$URL")
-
-# Calculate body size in bytes using 'wc'
-BODY_SIZE=$(echo -n "$RESPONSE" | wc -c)
-
-# Display body size in bytes
-echo "$BODY_SIZE"
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Check if URL argument provided
+# Check if URL argument is provided
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <URL>"
   exit 1
@@ -9,8 +9,12 @@ fi
 # Get URL from command line argument
 URL="$1"
 
-# Send GET request to URL and display body of response for status code 200
-if [ "$(curl -s -o /dev/null -w "%{http_code}" "$URL")" -eq 200 ]; then
+# Send GET request to URL and store response in variable
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+
+# Check the HTTP status code is 200
+if [ "$RESPONSE" -eq 200 ]; then
+  # Send another GET request to URL and display body of response
   curl -s "$URL"
 else
   echo "Error: HTTP status code is not 200"
