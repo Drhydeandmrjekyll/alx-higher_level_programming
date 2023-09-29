7eb12244972ad57f57527177a19dae69bbf82c30
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-09-28 17:28:01 -0700

    5000_port

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Script sends request to 0.0.0.0:5000/catch_me to get server's response message.
 
-curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -X GET -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
 
