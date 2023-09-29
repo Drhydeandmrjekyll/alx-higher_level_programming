77e791fa3619109935863182cbc828f8b3986847
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-09-28 18:28:45 -0700

    JSON

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Script sends JSON POST request to URL with contents of file as request body.
-if [ -f "$2" ]; then
-    curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
-fi
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
